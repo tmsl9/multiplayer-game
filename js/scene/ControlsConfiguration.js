70229065f0ec812c7a2830c06d3a17f228c5f986
Toni <36644@ufp.edu.pt>
2020-04-10 03:07:39 +0100

    100%

--- js/scene/ControlsConfiguration.js
+++ js/scene/ControlsConfiguration.js
@@ -64,7 +64,7 @@ export default class controlsConfiguration extends Phaser.Scene {
         but.on('pointerout', () => but.setStyle({ fill: '#fff'}))
     }
     
-    clickKey(pointer, buttons){//atenção quando clica no ecrã dá erro, arranjar solução com if ou assim
+    clickKey(pointer, buttons){
         if(buttons.length > 0){
             this.button = buttons[0]
             this.button.setStyle({ fill: '#0f0'})
